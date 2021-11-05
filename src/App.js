c9e99704a5a1f28e336df2d3bb9c718193251279
James Kirkwood <85250373+jimjamesjimathy@users.noreply.github.com>
2021-11-05 16:44:47 -0400

    All functionality is where I want it. finishing styles

--- src/App.js
+++ src/App.js
@@ -2,16 +2,33 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 import './App.css';
 import Character from './components/Character';
+import styled from 'styled-components';
+
+const Container = styled.div`
+width: 100%;
+height: 100vh;
+margin: 0 auto;
+`
+const Titleh1 = styled.h1`
+  font-family: 'Days One', sans-serif;
+	font-size: 9rem;
+	font-weight: 500;
+  color: var(--burlywood);
+  text-shadow: 2px 3px 2px #000;
+  letter-spacing: 1rem;
+  padding: 2% 0;
+`
+
+
 
 const App = () => {
-  const [character, setCharacter] = useState([]);
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  const [character, setCharacter] = useState([]);
+  const [allCharacters, setAllCharacters] = useState(false);
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
   useEffect(() => {
     axios.get('https://swapi.dev/api/people')
       .then(res => {
@@ -24,13 +41,10 @@ const App = () => {
   }, []);
 
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-        
-{ character.map(item => {
-  return <Character item={item} character={character}/>
-})}
-    </div>
+    <Container className="App">
+      <Titleh1 className="Header">Star Wars Characters</Titleh1>
+        { character.map(item => <Character item={item} />) }
+    </Container>
   );
 }
 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,27 +1,77 @@
 // Write your Character component here
 import React, { useState } from 'react';
+import styled from 'styled-components';
+import { useTransition, animated } from 'react-spring'; 
+
+const CharacterDiv = styled.div`
+font-family: 'Raleway', sans-serif;
+margin: 0 auto;
+padding: 1.5%;
+width: 50%;
+height: auto;
+background-color: var(--beauBlue);
+display: flex;
+flex-flow: row wrap;
+justify-content: space-around;
+align-items: center;
+border-radius: 5px;
+`
+const Name = styled.h3`
+font-family: 'Days One', sans-serif;
+font-size: 3rem;
+text-decoration: underline;
+padding: 2% 0;
+color: var(--text);
+text-shadow: 1px 2px 0px #000;
+`
+const InfoP = styled.p`
+font-size: 2rem;
+font-weight: 500;
+padding: 0 2%;
+`
+const Button = styled.button`
+width: 20%;
+`
 
 
 const Character = props => {
-  const [showFilms, setShowFilms] = useState(false);
+  const [showInfo, setShowInfo] = useState(false);
+  const [movies, setMovies] = useState(false);
 
-    const clicker = () => setShowFilms(showFilms ? false : true)
+
+  const transition = useTransition(showInfo, {
+    from: { x: 50, y: 0, opacity: 0 },
+    enter: { x: 0, y: 0, opacity: 1},
+    leave: { x: -50, y: 0, opacity: 0 },
+    config: {mass: 3, tension: 333, friction: 33}
+  });
+
+  const films = props.item.films;
+
     return (
-     <div>
-        <div>
-         <h3>{props.item.name}</h3>
-        </div>
-            <div>
-             <p>Gender: {props.item.gender}</p>
-             <p>Birth year: {props.item.birth_year}</p>
-             <p>Eye color:{props.item.eye_color}</p>
+     <CharacterDiv>
 
+         <Name>{props.item.name}</Name>
+         <Button onClick={() => {
+             setShowInfo(vis => !vis)
+         }}>{showInfo ? 'See Less' : 'See More'}</Button>
 
-<button type='submit' onClick={clicker}>Featured in</button>
-    {showFilms ? <p>{props.item.films.map(film =>  film)}</p> : false }
-
-            </div>
-        </div>
+            {transition((style, i) => 
+                i ? <animated.div className='sub-info' style={style}>
+                <InfoP>Birth year: <span>{props.item.birth_year}</span></InfoP>
+                <InfoP>Gender: <span>{props.item.gender}</span></InfoP>
+                <InfoP>Eye color: <span>{props.item.eye_color}</span></InfoP>
+                <InfoP>Hair Color: <span>{props.item.hair_color}</span></InfoP>
+                <InfoP>Mass: <span>{props.item.mass}</span></InfoP>
+                <InfoP>Height: <span>{props.item.height}</span></InfoP>
+                <button onClick={() => {
+                    setMovies(movies ? false : true);
+                }}>List Movies</button>
+                { movies ? <div><InfoP>{films.join(', ')}</InfoP></div> : '' }
+                </animated.div> : ''
+            )}
+            
+        </CharacterDiv>
     )
 }
 
